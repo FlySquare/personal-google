05b2e0ef5582724e8c232932d0e514db53fae5da
Umut Can Arda <umutkonurinso@gmail.com>
2020-06-03 04:04:17 +0300

    export

--- db/engine.php
+++ db/engine.php
@@ -1,8 +1,5 @@
 <?php
 if (isset($_POST['searchword'])) {
-  ?>
-<script type="text/javascript" language="Javascript">window.open('<?php echo "https://www.google.com/search?q=".$_POST['searchword']; ?>');</script>
-<?php
-
+header("Location: https://www.google.com/search?q=".$_POST['searchword']);
 }
  ?>
